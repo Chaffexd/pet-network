dc1671296f7986895d3a14648bcba8a5eea7fdc7
Shane Chaffe <67240269+Chaffexd@users.noreply.github.com>
2022-10-12 23:15:37 +0800

    Commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f606847ab4msh07b8c71f04d59b4p1ae0c2jsn8089a140c416',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GET_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "320a9ebf66acd152b0402f7f1ec7d9da";
